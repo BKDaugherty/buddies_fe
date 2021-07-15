c37432022336a403a53a73467022dad2a3005538
Brendon Daugherty <brendon1097@gmail.com>
2021-07-14 21:41:20 -0700

    some garbage

--- src/actions/buddies.js
+++ src/actions/buddies.js
@@ -1,5 +1,4 @@
 // This file creates a mapping of actions
-
 const actions = {
   create_buddy: "buddy/create",
 };
@@ -7,8 +6,9 @@ const actions = {
 // This function takes advantage of functional patterns to give a nice API for dispatching
 // actions. (Unsure if this is actually useful yet...)
 const action_generator = (action) => (data) => {
-  // The action generator relies on Redux Thunk Middleware to pass it these arguments
-    return (dispatch, getState) => {
-	return dispatch({
-    };
+  // The action generator will rely on Redux Thunk Middleware to pass it these arguments
+  // It certainly doesn't do anything useful now.
+  // return (dispatch, getState) => {
+  //  return dispatch({})
+  //   * }; */
 };
